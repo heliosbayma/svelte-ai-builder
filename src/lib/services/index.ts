35ff0eeace0feedf83632b74ebb2b43e65fc826f
Helios Bayma <heliosbayma@gmail.com>
2025-08-31 22:56:28 -0400

    feat: wire up LLM service exports and public API

---
+++ src/lib/services/index.ts
@@ -0,0 +1,4 @@
+export { llmClient, SYSTEM_PROMPT, createComponentPrompt } from './llm';
+export type { LLMMessage, LLMResponse, LLMStreamResponse, LLMOptions, LLMProviderType } from './llm';
+export { svelteCompiler } from './compiler';
+export type { CompileResult, CompileWarning, CompileError } from './compiler';
---
+++ src/lib/services/llm/index.ts
@@ -0,0 +1,3 @@
+export { llmClient } from './client';
+export { SYSTEM_PROMPT, createComponentPrompt } from './prompts';
+export type { LLMMessage, LLMResponse, LLMStreamResponse, LLMOptions, LLMProviderType } from './types';
